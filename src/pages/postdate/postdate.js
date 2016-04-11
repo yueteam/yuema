6d8642e7607f7b72fe5faa9d4ae3675a6a0cf35c
zhifeng.xzf <zhifeng.xzf@alibaba-inc.com>
2016-04-12 01:32:33 +0800

    完善发布约会

--- src/pages/postdate/postdate.js
+++ src/pages/postdate/postdate.js
@@ -1,15 +1,17 @@
 $(function() {
 	var Utils = require("../../common/utils");
-	var dialog = require("../../components/dialog/dialog");
-	var loading = require("../../components/loading/loading");
+	var Tips = require('../../components/tips');
+	var Dialog = require("../../components/dialog/dialog");
+	var Loading = require("../../components/loading/loading");
+	var Ajax = require('../../components/ajax');
 	
 	var app = {
     	init: function() {
     		$('#dateDesc').focus();
 
     		var listH = $(window).height() - 300;
-    		$('.what-block').height(listH);
-    		
+    		$('.opt-panel').height(listH);
+
     		this.initEvent();
     	},
 
@@ -23,6 +25,52 @@ $(function() {
                 // $('.post-option')[0].scrollIntoView(true);
                 
             });
+
+            $(document).on('tap', '.opt-item', function () {
+            	var $this = $(this),
+            		opt = $this.data('opt');
+
+            	if($this.hasClass('active')) {
+            		return false;
+            	}
+
+            	$('.post-option .active').removeClass('active');
+            	$this.addClass('active');
+            	$('.opt-panel.open').removeClass('open');
+            	$('.'+opt+'-block').addClass('open');
+            });
+
+        },
+
+        /**
+         * 提交数据
+         */
+        doSubmit: function () {
+            var me = this;
+
+            Loading.show();
+           
+            Ajax.post('http://test.yuema.us/open-api/regist', {
+            		'method': 'regist',
+					'userInfo': JSON.stringify(submitData)
+				},
+				function(d){
+					Loading.hide();
+
+				    Tips.show({
+	                    type: 'success',
+	                    title: '保存成功'
+	                });
+				},
+				function(d){
+					Loading.hide();
+
+				    Tips.show({
+	                    type: 'error',
+	                    title: d.resultMsg
+	                });
+				}
+			);
 
         }
     };
