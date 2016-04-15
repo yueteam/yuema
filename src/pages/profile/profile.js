0b46e7b4330a81660288788b212f97a0b3c41bd7
zhifeng.xzf <zhifeng.xzf@alibaba-inc.com>
2016-04-15 10:12:26 +0800

    完善个人主页

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -3,7 +3,7 @@ $(function() {
 	
 	var width = $(window).width();
 	$('.photo').height(width/2);
-	$('.tags').height(width/2);
+	$('.tags-con').height(width/2);
 
 	$('.details').addClass('open');
 });
