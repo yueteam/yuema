f0ed0249369a2f65307116c1e5dd615c2106fe1a
zhifeng.xzf <zhifeng.xzf@alibaba-inc.com>
2016-04-30 15:25:19 +0800

    联调登录接口

---
+++ src/common/apimap.js
@@ -0,0 +1,13 @@
+var apiPerfix = 'http://test.yuema.us:8888/';
+var Apimap = {
+    loginApi: apiPerfix + 'open-api/login',
+    registApi: apiPerfix + 'open-api/regist',
+    provincesListApi: apiPerfix + 'open-api/get/provinces-list',
+    citiesListApi: apiPerfix + 'open-api/get/cities-list',
+    addressesListApi: apiPerfix + 'open-api/get/addresses-list',
+    postDatingApi: apiPerfix + 'action/post/dating',
+    requestDatingApi: apiPerfix + 'action/request/dating',
+    acceptDatingApi: apiPerfix + 'action/accept/dating'
+};
+
+module.exports = Apimap;
