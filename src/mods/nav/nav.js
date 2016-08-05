fd9b9cb42ce9d31bf1ce63e7f574f28772f715d1
玄悟 <zhifeng.xzf@alibaba-inc.com>
2016-08-05 11:28:13 +0800

    修改左侧导航

--- src/mods/nav/nav.js
+++ src/mods/nav/nav.js
@@ -29,7 +29,7 @@ module.exports = {
 
         if(Utils.isLogin()) {
             var nick = Utils.getCookie('nick_name');
-            $('.menu-profile').attr('href', './profile.html');
+            $('.menu-profile .profile-lnk').attr('href', './profile.html');
             $('.menu-profile .login-info').text(nick);
         }
     }
